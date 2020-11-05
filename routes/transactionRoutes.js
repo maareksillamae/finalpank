069d90aa665d98167ad3eeb0e3846d011e2b88b1
maareksillamae <49565514+maareksillamae@users.noreply.github.com>
2020-11-05 12:52:10 +0200

    Add files via upload
    Fixed transaction routes error

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,38 +1,38 @@
-const router = require('express').Router();
-const accountModel = require('../models/Account');
-const { verifyToken, refreshBanksFromCentralBank } = require('../middlewares/middlewares');
-const transactionModel = require('../models/Transactions');
-const userModel = require('../models/User');
-const bankModel = require('../models/CentralBank');
-const fs = require('fs');
-const axios = require('axios');
-const jose = require('node-jose');
-const fetch = require('node-fetch');
+const router = require('express').Router(); const accountModel = require('../models/Account'); const { verifyToken, refreshBanksFromCentralBank } = require('../middlewares/middlewares'); const transactionModel = 
+require('../models/Transactions'); const sessionModel = require('../models/Sessions'); const userModel = require('../models/User'); const bankModel = require('../models/CentralBank'); const fs = require('fs'); const axios = 
+require('axios'); const jose = require('node-jose'); const fetch = require('node-fetch');
 
 require('dotenv').config();
 
-router.get('/transaction-history', verifyToken, async(req, res) => {
+router.get('/', verifyToken, async(req, res) => {
     try {
+// Get a specific users session token
+        const sessionId = req.headers.authorization.split(' ')[1]
 
+        // Find a session with the provided Id
+        const session = await sessionModel.findOne({ _id: sessionId });
 
         // Find the account associated with the user
-        const accountId = await accountModel.findOne({user: req.userId});
-
+        const accountId = await accountModel.findOne({user: session.userId});
+	console.log(accountId.accountNumber);
         // Find all transactions
-        const sentTransaction = await transactionModel.findOne({ user: accountId.userId })
+        const sentTransaction = await transactionModel.find({ userId: accountId.user })
+        
+        const receivedTransaction = await transactionModel.find({accountTo: accountId.accountNumber});
 
-        if (!sentTransaction) {
+        if (!sentTransaction || !receivedTransaction) {
             res.status(404).json({ error: "You have no logged transactions" });
-        } else {
+        } 
             console.log("Displaying transactions")
-            res.status(200).json({transactions: sentTransaction});
-        }
-
+            res.status(200).json({
+                transactions: sentTransaction,
+                received: receivedTransaction
+            });
     } catch (e) {
         return res.status(400).json({error: "error"})
     }
 })
-router.post('/create-transactions', verifyToken, async(req, res, next) => {
+router.post('/', verifyToken, async(req, res, next) => {
     let banks = [],
         statusDetail
     const loggedUserAccount = await accountModel.findOne({ accountNumber: req.body.accountFrom});
